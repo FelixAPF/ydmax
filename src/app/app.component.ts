d73e11429e574fd2b074348035c45a23b4199f11
Felix Pelletier <felix.pelletierf@hotmail.com>
2026-07-08 10:49:59 -0400

    update business offerings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = "Y-D/MAX FARC";
+  currentYear = new Date().getFullYear();
+  
+
+  scrollToSection(id: string): void {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+
+  selectedImage: string | null = null;
+
+  openLightbox(imageUrl: string) {
+    this.selectedImage = imageUrl;
+    document.body.style.overflow = 'hidden';
+  }
+
+  closeLightbox() {
+    this.selectedImage = null;
+    document.body.style.overflow = '';
+  }
 }
